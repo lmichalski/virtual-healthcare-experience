67e495164e378e95eec10210a80912dc46a07cab
Ryan Marks <ryanmarks151@gmail.com>
2021-09-25 10:31:21 -0400

    Add and run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
-import React from 'react'
+import React from "react";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { IntlProvider } from "react-intl";
 
-import Main from './pages/Main'
+import Main from "./pages/Main";
 
-import './App.scss'
+import "./App.scss";
 
-const enMessages = require("./lang-compiled/en.json") as Record<string, string>
-const frMessages = require("./lang-compiled/fr.json") as Record<string, string>
+import enMessages from "./lang-compiled/en.json";
+import frMessages from "./lang-compiled/fr.json";
 
-function App () {
-
-  const locale = 'fr' as string
-  const messages = locale === 'fr' ? frMessages : enMessages
+const App: React.FC<{}> = () => {
+  const locale = 'fr' as string;
+  const messages = locale === "fr" ? frMessages : enMessages;
 
   return (
     <IntlProvider messages={messages} locale={locale} defaultLocale="en">
       <Router>
-        <div className='fullscreen'>
-          <div className='view' role='application'>
+        <div className="fullscreen">
+          <div className="view" role="application">
             <Switch>
-              <Route path='/main'>
+              <Route path="/main">
                 <Main />
               </Route>
             </Switch>
@@ -33,7 +28,7 @@ function App () {
         </div>
       </Router>
     </IntlProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,29 +1,34 @@
+import { FormattedMessage } from "react-intl";
+import "./Main.scss";
 
-import { FormattedMessage } from 'react-intl'
-import './Main.scss'
+const Main: React.FC<{}> = () => {
+  let a = 1;
 
-const Main = () => {
   return (
-    <div className='container splash'>
-      <div className='panel'>
+    <div className="container splash">
+      <div className="panel">
         <header>
           <FormattedMessage
-            id='Main.title'
-            defaultMessage='Emergency'
-            description='Main page title'
-            tagName='h1'
+            id="Main.title"
+            defaultMessage="Emergency"
+            description="Main page title"
+            tagName="h1"
           />
         </header>
-        <div className='main'>
+        <div className="main">
           <p>
-            <a href='#/menu/' className='button'>
-              <FormattedMessage id='Main.playGameButton' defaultMessage='Play Game' description='Play game button text' />
+            <a href="#/menu/" className="button">
+              <FormattedMessage
+                id="Main.playGameButton"
+                defaultMessage="Play Game"
+                description="Play game button text"
+              />
             </a>
           </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
