2b383862ab6a43a7350d0ad4f5e7046592634029
Ryan Marks <ryanmarks151@gmail.com>
2021-09-25 00:00:09 -0400

    Add React Intl

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,39 @@
-import React from 'react';
+import React from 'react'
 
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
-} from "react-router-dom";
+  Route
+} from 'react-router-dom'
+import { IntlProvider } from "react-intl";
 
-import Main from './Main'
+import Main from './pages/Main'
 
-import logo from './logo.svg';
+import './App.scss'
 
-import './App.scss';
+const enMessages = require("./lang-compiled/en.json") as Record<string, string>
+const frMessages = require("./lang-compiled/fr.json") as Record<string, string>
 
-function App() {
+function App () {
+
+  const locale = 'fr' as string
+  const messages = locale === 'fr' ? frMessages : enMessages
+
   return (
-    <Router>
-      <div className="fullscreen">
-        <div className="view" role="application">
-        <Switch>
-            <Route path="/main">
-              <Main />
-            </Route>
-          </Switch>
+    <IntlProvider messages={messages} locale={locale} defaultLocale="en">
+      <Router>
+        <div className='fullscreen'>
+          <div className='view' role='application'>
+            <Switch>
+              <Route path='/main'>
+                <Main />
+              </Route>
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
-  );
+      </Router>
+    </IntlProvider>
+  )
 }
 
-export default App;
+export default App
