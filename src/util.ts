c5129c0696647568c2ad28122f30ddc127f2620f
Ryan Marks <ryanmarks151@gmail.com>
2021-10-06 18:32:00 -0400

    Add a working save with cookies

--- src/util.ts
+++ src/util.ts
@@ -88,7 +88,7 @@ export function getBrowser() {
 // Cookies + State, it's messy here
 //////////////////////////////////////////////////
 
-var expiry_days = 10;
+var maxAge = 21 * 24 * 60;
 
 function isLocalStorageAvailable() {
   var str = "test";
@@ -102,41 +102,57 @@ function isLocalStorageAvailable() {
 }
 
 const cookies = {
-  putString: (...args: any[]) => alert("Cookies don't work yet"),
-  get: (...args: any[]) => alert("Cookies don't work yet"),
-  putObject: (...args: any[]) => alert("Cookies don't work yet"),
-  getObject: (...args: any[]) => alert("Cookies don't work yet"),
-  remove: (...args: any[]) => alert("Cookies don't work yet"),
+  put(key: string, value: string, maxAge: number) {
+    var cookie = `${key}=${value};path=/;max-age=${maxAge};`
+    console.log(cookie)
+    document.cookie = cookie;
+  },
+  get(cname: string): string | null {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return null;
+  },
+  remove (key: string) {
+    this.put(key, "", -1)
+  },
 };
 
 export const Storage = {
   set: function (key: string, value: string) {
-    var d = new Date();
     if (isLocalStorageAvailable()) {
       window.localStorage.setItem(key, value);
     } else {
-      cookies.putString(key, value, { expires: d.setDate(expiry_days) });
+      cookies.put(key, value, maxAge);
     }
   },
-  get: function (key: string) {
+  get: function (key: string): string | null {
     var r = isLocalStorageAvailable()
       ? window.localStorage.getItem(key)
       : cookies.get(key);
     return r;
   },
   setObject: function (key: string, value: any) {
-    var d = new Date(),
-      o = JSON.stringify(value);
+    const o = JSON.stringify(value);
     if (isLocalStorageAvailable()) {
       window.localStorage.setItem(key, o);
     } else {
-      cookies.putObject(key, o, { expires: d.setDate(expiry_days) });
+      cookies.put(key, o, maxAge);
     }
   },
   getObject: function (key: string) {
     var r = isLocalStorageAvailable()
       ? window.localStorage.getItem(key)
-      : cookies.getObject(key);
+      : cookies.get(key);
     return r ? JSON.parse(r) : false;
   },
   remove: function (key: string) {
